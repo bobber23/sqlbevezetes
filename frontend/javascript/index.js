348dfbbbdc0e65df010076fe01c84b24941251b5
Botond Bugja <bugjabotond@gmail.com>
2026-02-18 14:26:00 +0100

    sfdgsfdgjkgbjkl

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -10,11 +10,26 @@ const diaktablazatfeltoltese = async () => {
             const tr = document.createElement('tr');
             const td1 = document.createElement('td');
             const td2 = document.createElement('td');
+            const td3 = document.createElement('td');
+            const button = document.createElement('button');
+            button.classList.add('btn', 'btn-primary');
+            button.setAttribute('id', element.nev);
+            button.innerHTML = 'Jegyek megjelenítése';
+            button.addEventListener('click', jegyekmegjelenitese);
             td1.innerHTML = element.nev;
             td2.innerHTML = element.osztaly;
-            tr.replaceChildren(td1, td2);
+            td3.replaceChildren(button);
+            tr.replaceChildren(td1, td2, td3);
             diakTablaBody.appendChild(tr);
         });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const jegyekmegjelenitese = async (e) => {
+    try {
+        console.log(e.target.id);
     } catch (error) {
         console.log(error);
     }
